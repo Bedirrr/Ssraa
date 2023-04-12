6e9a06764281b6032c78599ce6e0888f938a16d6
Bedirhan <albayrakbedirhan570@gmail.com>
2023-04-12 05:19:05 +0300

    Initial commit

---
+++ src/SICAKLIK.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SICAKLIK {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+
+
+    }
+}
---
+++ src/UcgenCevre.java
@@ -0,0 +1,2 @@
+public class UcgenCevre {
+}
